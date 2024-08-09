dce686a0d0fe372ee4d084a6cba1cb038ed42db9
AlinaZakharovadev <zakharova.alina.design@gmail.com>
2024-08-09 13:56:44 +0200

    issue with Leaflet has been fixed

--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -1,6 +1,22 @@
 import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+const iconUrl = "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png";
+const iconShadowUrl =
+  "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: iconUrl,
+  shadowUrl: iconShadowUrl,
+  iconSize: [25, 42], // Размер иконки
+  iconAnchor: [12, 41], // Точка привязки
+  shadowSize: [41, 41], // Размер тени
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function Pin({ item }) {
   return (
